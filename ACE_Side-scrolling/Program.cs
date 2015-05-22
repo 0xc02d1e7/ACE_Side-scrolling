df62026224f279b03c4ec237bae27999c8e23a5d
wisteria <wisteria@coins.tsukuba.ac.jp>
2015-05-22 10:51:43 +0900

    レイヤーを追加(プレイヤーと背景)

--- ACE_Side-scrolling/Program.cs
+++ ACE_Side-scrolling/Program.cs
@@ -62,8 +62,21 @@ namespace ACE_Test
             // AC-Engineを初期化する。
             ace.Engine.Initialize("Empty", 640, 480, new ace.EngineOption());
 
+            ace.Scene scene =new ace.Scene();
+
+            ace.Layer2D layer=new ace.Layer2D();
             Player player = new Player();
-            ace.Engine.AddObject2D(player);
+            layer.DrawingPriority = 2;
+            layer.AddObject(player);
+            scene.AddLayer(layer);
+
+            ace.Layer2D backlayer = new ace.Layer2D();
+            ace.TextureObject2D back = new ace.TextureObject2D();
+            back.Texture = ace.Engine.Graphics.CreateTexture2D("Resources/back.png");
+            backlayer.AddObject(back);
+            scene.AddLayer(backlayer);
+
+            ace.Engine.ChangeScene(scene);
 
             // AC-Engineが進行可能かチェックする。
             while (ace.Engine.DoEvents())
