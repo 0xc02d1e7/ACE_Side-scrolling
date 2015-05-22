0f4a21967a51407d73b4e2253eb91b5d3849992c
wisteria <wisteria@coins.tsukuba.ac.jp>
2015-05-22 18:07:39 +0900

    ジャンプ機能付けた

--- ACE_Side-scrolling/Program.cs
+++ ACE_Side-scrolling/Program.cs
@@ -8,8 +8,9 @@ namespace ACE_Test
 {
     class Player : ace.TextureObject2D
     {
-        int count = 0, anime = 0;
-        ace.Texture2D[] bouningen = new ace.Texture2D[4];
+        int count = 0, anime = 0, status = 0;
+        int Vspeed=0;
+        ace.Texture2D[] bouningen = new ace.Texture2D[6];
         ace.Vector2DF pos;
         public Player()
         {
@@ -17,40 +18,64 @@ namespace ACE_Test
             {
                 bouningen[i] = ace.Engine.Graphics.CreateTexture2D("Resources/W" + i.ToString() + ".png");
             }
-
+            for (int i = 0; i < 2; i++)
+            {
+                bouningen[i+4] = ace.Engine.Graphics.CreateTexture2D("Resources/J" + i.ToString() + ".png");
+            }
+            Texture = bouningen[0];
             Position = pos = new ace.Vector2DF(320.0f, 240.0f);
         }
 
         protected override void OnUpdate()
         {
-            if (ace.Engine.Keyboard.GetKeyState(ace.Keys.Left) == ace.KeyState.Hold)
+            if (status == 0)
             {
-                if (count % 5 == 0)
+                Texture = bouningen[anime % 4];
+                if (ace.Engine.Keyboard.GetKeyState(ace.Keys.Left) == ace.KeyState.Hold)
                 {
-                    anime++;
-                    pos.X -= 3;
-                    Position = pos;
+                    if (count % 4 == 0) anime++;
+                    pos.X--;
                     TurnLR = false;
-                }
+                    
 
-            }
-            else if (ace.Engine.Keyboard.GetKeyState(ace.Keys.Right) == ace.KeyState.Hold)
-            {
-                if (count % 5 == 0)
+                }
+                if (ace.Engine.Keyboard.GetKeyState(ace.Keys.Right) == ace.KeyState.Hold)
                 {
-                    anime++;
-                    pos.X += 3;
-                    Position = pos;
+                    if (count % 4 == 0) anime++;
+                    pos.X++;
                     TurnLR = true;
                 }
 
+                if(ace.Engine.Keyboard.GetKeyState(ace.Keys.Up) == ace.KeyState.Hold)
+                {
+                    status = 1;
+                    anime = 0;
+                    Texture = bouningen[4];
+                }
+            }
+            else if (status == 1 && count%5==0)
+            {
+                status = 2;
+                Vspeed = 8;
+                Texture = bouningen[5];
+            }
+
+            
+            
+            if (pos.Y > 240.0 && Vspeed<0)//床に触っているかの判定で置き換える
+            {
+                status = 0;
+                Vspeed = 0;
             }
             else
             {
-                anime = 0;
+                pos.Y -= Vspeed;
+                Vspeed -= 1;
             }
+
+            Position = pos;
             count++;
-            Texture = bouningen[anime % 4];
+            
         }
     }
 
